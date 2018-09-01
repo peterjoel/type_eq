d4ba62883da97ce5a1d7658e9e849aa8a30a08b4
Peter <peterjoel@gmail.com>
2018-09-01 21:24:50 +0100

    Removed requirement for Has::get to return a reference. Improved docs

--- examples/type_properties.rs
+++ examples/type_properties.rs
@@ -8,26 +8,26 @@ struct Point<T> { x: T, y: T }
 
 impl<T: Copy> Has<X> for Point<T> {
     type Val = T;
-    fn get<P>(&self) -> &Self::Val
+    fn get<P>(&self) -> Self::Val
     where
         Constrain: TypeEq<X, P> 
     {
-        &self.x
+        self.x
     }
 }
 
 impl<T: Copy> Has<Y> for Point<T> {
     type Val = T;
-    fn get<P>(&self) -> &Self::Val
+    fn get<P>(&self) -> Self::Val
     where
         Constrain: TypeEq<Y, P> 
     {
-        &self.y
+        self.y
     }
 }
 
 fn main() {
     let p = Point { x: 3, y: 5 };
-    assert_eq!(&3, p.get::<X>());
-    assert_eq!(&5, p.get::<Y>());
+    assert_eq!(3, p.get::<X>());
+    assert_eq!(5, p.get::<Y>());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -22,15 +22,17 @@
 ///     fn a_eq_t<T>(&self, _t: T) where Constrain: TypeEq<A, T> { }
 /// 
 ///     // This method must be called with an explicit type. Since T is contrained to
-///     // be the same type as A and B, this can allow the caller a more conveniet way of 
+///     // be the same type as A and B, this can allow the caller a more convenient way of 
 ///     // determining those types 
 ///     fn infer_a<T>(&self) where Constrain: TypeEq<A, T> + TypeEq<B, T> { }
 /// }
 /// ```
 pub struct Constrain;
 
-/// There are no situations where you should implement this trait except for the one
-/// impl for `Constrain`, which connects all 
+/// A trait that determines that two type parameters are the same type.
+/// There is only one implementation of this trait, `Constrain`, which ensures that all types 
+/// satisfy the condition of being equal to themselves. There should not be a reason to implement 
+/// this for other types, which is why it is marked as `unsafe`.
 pub unsafe trait TypeEq<A, B> {
     fn as_eq(&self, a: A) -> B;
 }
@@ -39,10 +41,35 @@ unsafe impl<A> TypeEq<A, A> for Constrain {
     fn as_eq(&self, a: A) -> A { a }
 }
 
-/// Type-level properties
+/// Type-level properties. This trait can be used to associate marker types with properties of
+/// the implementating type.
+/// 
+/// # Examples
+/// ```
+/// use type_eq::{Constrain, Has, TypeEq};
+/// 
+/// // marler type for the "name" property
+/// struct Name;
+/// 
+/// struct Foo;
+/// 
+/// impl Has<Name> for Foo {
+///     type Val = &'static str;
+///     fn get<P>(&self) -> Self::Val 
+///     where
+///         Constrain: TypeEq<Name, P>,
+///     {
+///         "Foo"
+///     }
+/// }
+/// 
+/// assert_eq!("Foo", Foo.get::<Name>());
+/// 
+/// ```
+/// 
 pub trait Has<Prop> {
-    type Val;
-    fn get<P>(&self) -> &Self::Val
+    type Val: Sized;
+    fn get<P>(&self) -> Self::Val
     where
         Constrain: TypeEq<Prop, P>;
 }
